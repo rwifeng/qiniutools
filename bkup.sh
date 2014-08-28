ece49912253ce66ef6c04534b9591ad8743143b4
rwifeng <rw.ifeng@gmail.com>
2014-08-28 22:35:22 +0800

    add  backup trigger

---
+++ bkup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+bucket="rwxf"
+prefix=""
+limit=10
+mkr=''
+
+#Usage: qboxrsctl listprefix <bucket> <prefix> [<limit>] [<marker>]
+IFS=$'\n'
+while true; do
+	fns=$(qrsctl listprefix $bucket "" $limit $mkr)
+	for fn in $fns
+	do
+		if [[ $fn == marker:* ]];then
+			mkr=$(sed 's/marker:[ \t]*//' <<<$fn) 
+		else
+			echo "http://127.0.0.1:51234/get?key=$fn"
+			curl -v "http://127.0.0.1:51234/get?key=$fn" >> backup.log 2>&1
+			if [[ $? != 0 ]];then
+				echo "ERR===>$fn" 
+			else
+				echo "OK===>$fn"
+			fi
+
+		fi
+	done 
+	
+	if [[ $mkr == '' ]]; then
+		exit 200
+	fi
+done
